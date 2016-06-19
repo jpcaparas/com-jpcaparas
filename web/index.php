23284fd78961d0af7f402e83317769818244a0c9
JP Caparas <jp.caparas@sandfield.co.nz>
2016-06-20 00:01:07 +1200

    Added robots to headers

--- web/index.php
+++ web/index.php
@@ -3,17 +3,23 @@
 require_once( dirname( __FILE__ ) . '/../vendor/autoload.php' );
 
 use App\Controllers;
-use Silex\Provider as SilexProvider;
 use App\Provider as AppProvider;
+use Silex\Provider as SilexProvider;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 $dotEnv = new \Dotenv\Dotenv( dirname( __DIR__ ) );
 $dotEnv->load();
 
-$app = new Silex\Application();
-
-$app['root_dir'] = dirname( __DIR__ );
-$app['request']  = $app->factory( function ( $app ) {
+$app                = new Silex\Application();
+$app['root_dir']    = dirname( __DIR__ );
+$app['request']     = $app->factory( function ( $app ) {
 	return $app['request_stack']->getCurrentRequest();
+} );
+$app['environment'] = $app->factory( function ( $app ) {
+	$env = getenv( 'ENVIRONMENT' ) ?: 'production';
+
+	return strtolower( $env );
 } );
 
 // Provider
@@ -38,6 +44,13 @@ $app->register(
 		'twig.path' => $app['root_dir'] . '/views'
 	]
 );
+
+// Robots
+$app->after( function ( Request $request, Response $response ) use ( $app ) {
+	if ( $app['environment'] != 'production' ) {
+		$response->headers->set( 'X-Robots-Tag', 'noindex, nofollow' );
+	}
+} );
 
 $app->match( '/', function () use ( $app ) {
 	return 'test';
